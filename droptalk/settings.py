c5bcd7059180f342fb08b25a76005115eb630baa
divnaoki <naokiimai1@gmail.com>
2025-03-16 12:08:14 +0900

    3/16 vercel

--- droptalk/settings.py
+++ droptalk/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- droptalk/wsgi.py
+++ droptalk/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'droptalk.settings')
 
 application = get_wsgi_application()
+
+# Vercel用の設定
+app = application
